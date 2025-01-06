2f5d70ed42ba0a6c0323482f0079adc9e647d319
fileuplaoderbot <tparveen2728@gmail.com>
2025-01-06 13:09:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
+API_ID = int(getenv("API_ID", "25101316"))
+API_HASH = getenv("API_HASH", "7a55428e826bc8557889f78a2e1af211")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "7990447298").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://Cluster0:Cluster0@cluster0.yhyvn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_GROUP = getenv("LOG_GROUP", "")
 CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
